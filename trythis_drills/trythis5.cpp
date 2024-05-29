a8cdb2c7b916894a431b0184074eeedf8ccbed9a
Katie Barrett <kayt.barrett22@gmail.com>
2024-05-29 08:20:08 -0600

    PPP ch3, try this 5 complete but not optimized

---
+++ trythis_drills/trythis5.cpp
@@ -0,0 +1,29 @@
+// Implement square() without using the multiplication operator
+// that is, do the x * x by repeated addition 
+// (start a variable at 0 and add x to it x amount of times)
+
+#include <iostream>
+
+using namespace std;
+
+
+int main () {
+
+    cout << "Please enter a whole number you want the square of:" << endl;
+    int numberToSquare;
+    cin >> numberToSquare;
+    int squaredNumber = 0;
+    int currentNumber = 0;
+    int i = 0;
+
+    while (i < numberToSquare) {
+        squaredNumber = currentNumber + numberToSquare;
+        currentNumber = squaredNumber;
+        i++;
+    }
+
+    cout << "Your number squared is " << squaredNumber << endl;
+
+
+    return 0;
+}
